613731350c401481354370ce593029798279775a
Jeferson <mx.jeferson.12@gmail.com>
2019-07-10 00:04:23 -0300

    Add youtube robot

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const robots = {
 	text: require('./robots/text.js'),
 	state: require('./robots/state.js'),
 	image: require('./robots/image.js'),
-	video: require('./robots/video.js')
+	video: require('./robots/video.js'),
+	youtube: require('./robots/youtube.js')
 }
 
 async function start(){
@@ -13,9 +14,10 @@ async function start(){
 	//robots.input()
 	//await robots.text()
 	//await robots.image()
-	await robots.video()
+	//await robots.video()
+	await robots.youtube()
 
-	const content = robots.state.load()
+	//const content = robots.state.load()
 	//console.dir(content, { depth: null })
 }
 
